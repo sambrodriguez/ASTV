749fea1372f87ef284f6833fef12c87169e42d10
Samuel Brodersen-Rodriguez <broderse@stolaf.edu>
2016-05-19 19:51:35 -0500

    added a bunch to gui, and added stringsplit and help.txt

--- gui.rkt
+++ gui.rkt
@@ -1,10 +1,15 @@
 #lang racket
 (require "draw.rkt")
 (require "defined.rkt")
+(require "stringsplit.rkt")
 (require racket/gui/base)
 (require (lib "eopl.ss" "eopl"))
 
+(define strh (file->string "help.txt"))
+
 (define treef (new frame% [label "Visualed Tree"][width 800][height 800]))
+
+(define helpf (new frame% [label "Help Documentation"][width 600][height 600]))
 
 (define canvasdraw2
   (lambda (canvas dc)
@@ -12,17 +17,45 @@
 
 (define start (new frame% [label "Input"] [width 800][height 800]))
 
+(define title-font (make-font #:size 32 #:family 'swiss
+                            #:weight 'bold))
+
 (define title (new message% [parent start]
+                   [label "Abstract Syntax Tree Visualizer"]
+                   [font title-font]))
+
+(define prompt (new message% [parent start]
                           [label "Input expression to be visualized"]))
 
-(define textbox (new text-field% [label "expression"] [parent start]))
+(define textbox (new text-field% [label "Expression:"] [parent start]))
 
-(new button% [parent start]
+(define panel (new horizontal-panel% [parent start]
+                   [alignment '(center center)]))
+
+(new button% [parent panel]
              [label "Draw Tree"]
              ; Callback procedure for a button click:
              [callback (lambda (button event)
                          (send treef show #t))])
-                    
+
+(new button% [parent panel]
+             [label "Help!"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (send helpf show #t))])
+
+
+
+#|(define helpc (new canvas% [parent helpf]
+     ;[paint-callback helpdraw]
+     ))|#
+
+(define drawhelp
+  (lambda (canvas dc)
+    (send dc draw-text strh 0 0)))
+
+(define helptext (new canvas% [parent helpf]
+                          [paint-callback drawhelp]))
 
 (define treec (new canvas% [parent treef]
      [paint-callback canvasdraw2]))
