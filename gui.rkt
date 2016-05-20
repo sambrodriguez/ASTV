7d1161cb92ad83d97f3a8e713c64efe9ea691b8d
Samuel Brodersen-Rodriguez <broderse@stolaf.edu>
2016-05-19 22:16:49 -0500

    fixed help button

--- gui.rkt
+++ gui.rkt
@@ -16,7 +16,7 @@
 
 (define treef (new frame% [label "Visualized Tree"][width 800][height 800]))
 
-(define helpf (new frame% [label "Help Documentation"][width 600][height 600]))
+;(define helpf (new frame% [label "Help Documentation"][width 600][height 600]))
 
 (define canvasdraw2
   (lambda (canvas dc)
@@ -56,11 +56,6 @@
              [callback (lambda (button event)
                          (send f6 show #t))])
 
-(new button% [parent panel]
-             [label "Help!"]
-             ; Callback procedure for a button click:
-             [callback (lambda (button event)
-                         (send helpf show #t))])
 
 ;Add buttons that spawn images
 (define panel2 (new horizontal-panel% [parent start]
@@ -189,16 +184,47 @@
 (define f12 (new frame% [label "Bitmap"]))
 (new message% [parent f12] [label callBit])
 
-#|(define helpc (new canvas% [parent helpf]
-     ;[paint-callback helpdraw]
-     ))|#
+(new button% [parent panel]
+             [label "Help!"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (send tutFrame show #t))])
 
-(define drawhelp
-  (lambda (canvas dc)
-               (send dc draw-text strh 0 0)))
 
-(define helptext (new canvas% [parent helpf]
-                          [paint-callback drawhelp]))
+(define tutFrame (new frame% [label "Tutorial"] [width 800][height 300]))
+
+(define tutTitle (new message% [parent tutFrame]
+                          [label "Welcome to the Abstract Syntax Tree Visualizer!"]))
+
+(define tutTitle2 (new message% [parent tutFrame]
+                          [label ""]))
+
+(define tutTextWelcome (new message% [parent tutFrame]
+                          [label "This is the program where you get to visualize abstract syntax trees in exchange for entering the concrete syntax into the textbox, and pressing the Draw Tree button. "]))
+
+(define tutTextWelcome2 (new message% [parent tutFrame]
+                             [label "Our program currently supports the LET language from the book, as well as the Proc language from EOPL."]))
+
+(define tutTextWelcome3 (new message% [parent tutFrame]
+                             [label "For example, try entering the following into the textbox, without quotes: let x = 5 in -(x,3)"]))
+
+(define tutTextWelcome4 (new message% [parent tutFrame]
+                             [label "If you entered it correctly, you will see a tree! Congratulations! You have successfully run the program."]))
+
+(define tutTextWelcome8 (new message% [parent tutFrame]
+                             [label "Notice that in addition to the tree, there is also black text that shows the result of evaluation of your entered program"]))
+
+(define tutTextWelcome5 (new message% [parent tutFrame]
+                             [label "Now that you know how to run the program, try a few harder examples. Here are a few to choose from:"]))
+
+(define tutTextWelcome6 (new message% [parent tutFrame]
+                             [label "let f = proc(x) +(x,5) in (f 6)"]))
+
+(define tutTextWelcome7 (new message% [parent tutFrame]
+                             [label "(proc(x) +(x,5) 10)"]))
+
+(define tutTextWelcome9 (new message% [parent tutFrame]
+                             [label "Best of luck to you, and thank you for using our program!"]))
 
 (define treec (new canvas% [parent treef]
      [paint-callback canvasdraw2]))
